e9aca5f6e27b00c934b7ecbbbcec09938ad627fd
dasilvakevyn <kevyn.herbert@proton.me>
2025-09-23 21:30:18 -0300

    criando a logica do projeto

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const listaAmigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+
+    const amigo = input.value.trim();
+
+    if (amigo === '') {
+        alert('Você não inseriu um nome!');
+        return;
+    }
+
+    listaAmigos.push(amigo);
+    AtualizarLista();
+    input.value = '';
+    input.focus();
+}
+
+function AtualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    listaAmigos.forEach(function(amigo, i) {
+        const item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    })
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length < 2) {
+        alert('A lista precisa ter, pelo menos, 2 amigos.');
+        return;
+    }
+
+    const randomizar = Math.floor(Math.random() * listaAmigos.length);
+    
+    const sorteado = listaAmigos[randomizar];
+
+    const resultado = document.getElementById('resultado');
+
+    resultado.innerHTML = '<li> Sorteado: <strong>' + sorteado + '</strong></li>';
+}
